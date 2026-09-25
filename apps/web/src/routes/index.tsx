6b90f82fe71dee27a0f8d029ad602cb38b255881
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2026-09-26 03:58:34 +0700

    fix: pengkondisian yang lebih tepat

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -153,7 +153,11 @@ function DashboardHomePage() {
 						if (lane.status && lane.status !== "OK") {
 							hasilAkhir = lane.status; // Cetak DSQ/DNF
 						} else if (!lane.finalTime) {
-							hasilAkhir = "DNS"; // Jika OK tapi waktu kosong, paksa jadi DNS
+						  if (ht.status === "FINISHED" || ht.status === "STOPPED") {
+								hasilAkhir = "DNS"; // Lomba selesai tapi tidak ada waktu = tidak hadir
+							} else {
+								hasilAkhir = "-"; // Lomba belum/sedang berjalan
+							}
 						}
 
 						sheet.addRow([
